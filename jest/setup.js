45971eb426f0513a5381922dbfc4352173bdbc37
Ilê Caian <ile.caian@gmail.com>
2021-10-13 10:05:28 -0300

    refactor: remove unnecessary call to global function

--- jest/setup.js
+++ jest/setup.js
@@ -14,9 +14,3 @@ jest.mock('react-native-reanimated', () => {
 jest.mock("react-native/Libraries/Animated/NativeAnimatedHelper");
 
 jest.useFakeTimers('legacy');
-
-const FRAME_TIME = 10;
-
-global.requestAnimationFrame = (callback) => {
-  setTimeout(callback, FRAME_TIME);
-};
